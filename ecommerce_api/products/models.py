e4e3a0a426a10923292b14ad029519cd8fcd8f8b
nikunjvaghasiya <nikunjvaghasiya638@gmail.com>
2024-05-04 22:24:29 +0530

    create project and implemtn crud for category and product.

---
+++ ecommerce_api/products/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
